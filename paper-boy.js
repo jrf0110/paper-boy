6ed1c819e40b6909a4e0d97d372e2bef6cb3586e
John Fawcett <jrf0110@gmail.com>
2016-09-04 18:28:40 -0500

    Fix constructor exports

--- paper-boy.js
+++ paper-boy.js
@@ -1,128 +1,114 @@
-(function(){
-  if (typeof module === 'object' && typeof define !== 'function') {
-    var define = function (factory) {
-      module.exports = factory(require, exports, module);
-    };
+var constructor = module.exports = function(routes){
+  this.enter = {};
+  this.exit = {};
+  this.scopes = {};
+  this.routeTokens = {};
+  this.paramIndices = {};
+  for (var route in routes){
+    if (typeof routes[route] === "object"){
+      if (routes[route].hasOwnProperty('enter')) this.addRoute(route, 'enter', routes[route].enter)
+      if (routes[route].hasOwnProperty('exit')) this.addRoute(route, 'exit', routes[route].exit);
+    } else this.addRoute(route, 'enter', routes[route]);
   }
-  
-  var PaperBoy = (function(){
+  this.silent = false;
+};
 
-    var constructor = function(routes){
-      this.enter = {};
-      this.exit = {};
-      this.scopes = {};
-      this.routeTokens = {};
-      this.paramIndices = {};
-      for (var route in routes){
-        if (typeof routes[route] === "object"){
-          if (routes[route].hasOwnProperty('enter')) this.addRoute(route, 'enter', routes[route].enter)
-          if (routes[route].hasOwnProperty('exit')) this.addRoute(route, 'exit', routes[route].exit);
-        } else this.addRoute(route, 'enter', routes[route]);
-      }
-      this.silent = false;
-    };
+var _utils = constructor._utils = {
+  extractRouteTokens: function(url){
+    return url.split('/').map(function(val, index){
+      var token = {
+        type: val[0] == ':' ? 'variable' : 'text'
+      , index: index
+      };
 
-    var _utils = constructor._utils = {
-      extractRouteTokens: function(url){
-        return url.split('/').map(function(val, index){
-          var token = {
-            type: val[0] == ':' ? 'variable' : 'text'
-          , index: index
-          };
+      if (token.type == 'variable') token.value = val.substring(1);
+      else token.value = val;
 
-          if (token.type == 'variable') token.value = val.substring(1);
-          else token.value = val;
+      return token;
+    });
+  }
+};
 
-          return token;
-        });
-      }
-    };
+constructor.prototype = {
+  addRoute: function(route, type, fn){
+    if (typeof type === "function"){
+      fn = type;
+      type = "enter";
+    }
+    this[type][route] = fn;
+    this._addScope(route);
+    this.routeTokens[route] = _utils.extractRouteTokens(route);
+    return this;
+  }
+, navigate: function(route, silent){
+    this.silent = !!silent;
+    window.location.hash = route;
+    return this;
+  }
+, listen: function(route, silent){
+    window.addEventListener('hashchange', this._onHashChange.bind(this));
+    this._onHashChange();
+    // For now lets not add the ability to enter a route here
+    // this.navigate((typeof route === "string") ? route : window.location.hash, silent);
+    return this;
+  }
+, stop: function(){
+    window.removeEventListener('hashchange', this._onHashChange);
+    return this;
+  }
+  // This is a hacked together mess. And I don't give a damn
+, _onHashChange: function(event){
+    var hash = window.location.hash.replace(/\/*#\/*/, "");
+    if (!this.silent){
+      if (this.exit.hasOwnProperty(this.current)) this.exit[this.current].call(this.scopes[hash]);
+      if (this.enter.hasOwnProperty(hash)){
+        this.enter[hash].call(this.scopes[hash]);
+      } else {
+        var exitTokens = _utils.extractRouteTokens(this.current || "");
+        var enterTokens = _utils.extractRouteTokens(hash);
 
-    constructor.prototype = {
-      addRoute: function(route, type, fn){
-        if (typeof type === "function"){
-          fn = type;
-          type = "enter";
+        // Check each route tokens for a match
+        var filteredTokens = [], url, args;
+
+        // Exit route
+        for ( url in this.routeTokens ){
+          args = [];
+          if (
+            url in this.exit &&
+            this.routeTokens[ url ].filter( function( token, index ){
+              // Filter out variables
+              if ( token.type == 'variable' ) filteredTokens.push( index );
+              return token.type != 'variable';
+            }).map(function(t){ return t.value; }).join('/') == exitTokens.filter( function( token, index ){
+              // Filter tokens that would match the positions of variables
+              if ( filteredTokens.indexOf( index ) > -1 ) args.push( token.value );
+              return filteredTokens.indexOf( index ) == -1;
+            }).map(function(t){ return t.value; }).join('/')
+          ){ this.exit[url].apply(this.scopes[url], args); break; }
         }
-        this[type][route] = fn;
-        this._addScope(route);
-        this.routeTokens[route] = _utils.extractRouteTokens(route);
-        return this;
-      }
-    , navigate: function(route, silent){
-        this.silent = !!silent;
-        window.location.hash = route;
-        return this;
-      }
-    , listen: function(route, silent){
-        window.addEventListener('hashchange', this._onHashChange.bind(this));
-        this._onHashChange();
-        // For now lets not add the ability to enter a route here
-        // this.navigate((typeof route === "string") ? route : window.location.hash, silent);
-        return this;
-      }
-    , stop: function(){
-        window.removeEventListener('hashchange', this._onHashChange);
-        return this;
-      }
-      // This is a hacked together mess. And I don't give a damn
-    , _onHashChange: function(event){
-        var hash = window.location.hash.replace(/\/*#\/*/, "");
-        if (!this.silent){
-          if (this.exit.hasOwnProperty(this.current)) this.exit[this.current].call(this.scopes[hash]);
-          if (this.enter.hasOwnProperty(hash)){
-            this.enter[hash].call(this.scopes[hash]);
-          } else {
-            var exitTokens = _utils.extractRouteTokens(this.current || "");
-            var enterTokens = _utils.extractRouteTokens(hash);
 
-            // Check each route tokens for a match
-            var filteredTokens = [], url, args;
-
-            // Exit route
-            for ( url in this.routeTokens ){
-              args = [];
-              if (
-                url in this.exit &&
-                this.routeTokens[ url ].filter( function( token, index ){
-                  // Filter out variables
-                  if ( token.type == 'variable' ) filteredTokens.push( index );
-                  return token.type != 'variable';
-                }).map(function(t){ return t.value; }).join('/') == exitTokens.filter( function( token, index ){
-                  // Filter tokens that would match the positions of variables
-                  if ( filteredTokens.indexOf( index ) > -1 ) args.push( token.value );
-                  return filteredTokens.indexOf( index ) == -1;
-                }).map(function(t){ return t.value; }).join('/')
-              ){ this.exit[url].apply(this.scopes[url], args); break; }
-            }
-
-            // Enter route
-            for ( url in this.routeTokens ){
-              args = [];
-              if (
-                url in this.enter &&
-                this.routeTokens[ url ].filter( function( token, index ){
-                  // Filter out variables
-                  if ( token.type == 'variable' ) filteredTokens.push( index );
-                  return token.type != 'variable';
-                }).map(function(t){ return t.value; }).join('/') == enterTokens.filter( function( token, index ){
-                  // Filter tokens that would match the positions of variables
-                  if ( filteredTokens.indexOf( index ) > -1 ) args.push( token.value );
-                  return filteredTokens.indexOf( index ) == -1;
-                }).map(function(t){ return t.value; }).join('/')
-              ){ this.enter[url].apply(this.scopes[url], args); break; }
-            }
-          }
+        // Enter route
+        for ( url in this.routeTokens ){
+          args = [];
+          if (
+            url in this.enter &&
+            this.routeTokens[ url ].filter( function( token, index ){
+              // Filter out variables
+              if ( token.type == 'variable' ) filteredTokens.push( index );
+              return token.type != 'variable';
+            }).map(function(t){ return t.value; }).join('/') == enterTokens.filter( function( token, index ){
+              // Filter tokens that would match the positions of variables
+              if ( filteredTokens.indexOf( index ) > -1 ) args.push( token.value );
+              return filteredTokens.indexOf( index ) == -1;
+            }).map(function(t){ return t.value; }).join('/')
+          ){ this.enter[url].apply(this.scopes[url], args); break; }
         }
-        this.current = hash;
-        this.silent = false;
       }
-    , _addScope: function(route){
-        this.scopes[route] = { _router: this, params: {} };
-      }
-    };
-    return constructor;
-  })();
-  if ('define' in window) define(function(){ return PaperBoy; });
-  else window.PaperBoy = PaperBoy;
-})();
+    }
+    this.current = hash;
+    this.silent = false;
+  }
+, _addScope: function(route){
+    this.scopes[route] = { _router: this, params: {} };
+  }
+};
